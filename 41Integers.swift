169332ea0690d2e762ce10bd0bcf6a16fcf3124b
Edgar Adrián <36165300+edgaradrian@users.noreply.github.com>
2018-02-20 22:38:39 -0600

    Swift exercise
    Example Integers

---
+++ 41Integers.swift
@@ -0,0 +1,11 @@
+import Cocoa
+
+print("El maximo valor para Int es \(Int.max)")
+print("El minimo valor para Int es \(Int.min)")
+print("El maximo valor de Int 32 bit es \(Int32.max)")
+print("El minimo valor de Int 32 bit es \(Int32.min)")
+
+print("El maximo valor para UInt es \(UInt.max)")
+print("EL minimo valor para UInt es \(UInt.min)")
+print("El maximo valor para Uint 32 bit \(UInt32.max)")
+print("El minimo valor para UInt 32 bit \(UInt32.min)")
