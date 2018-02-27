c9691a1616fd9fbcdd0665c7f00af18cd63dd8e2
Edgar Adrián <36165300+edgaradrian@users.noreply.github.com>
2018-02-26 23:41:16 -0600

    Swift Cadenas
    String basico

---
+++ StringEjemplos.swift
@@ -0,0 +1,23 @@
+//String ejemplos
+
+import Cocoa
+
+//String declarado como literal
+var cadenaA = "Hola Swift desde literal"
+print( cadenaA )
+
+//String como instancia
+var cadenaB = String("Hola Swift desde instancia")
+print( cadenaB )
+
+//String multilinea
+var cadenaC = """
+Hola Swift desde
+un cadena que es mutilineas.
+Adios
+"""
+
+print(cadenaC)
+
+	
+
