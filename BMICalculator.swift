f5c24a979e577edf98d609129af6e38f3bf3155e
Edgar <addick@ADRiMac.local>
2018-05-14 20:36:35 -0500

    BMI Calculator exercise

---
+++ BMICalculator.swift
@@ -0,0 +1,22 @@
+//BMICalculator
+
+import UIKit
+
+
+func getBMI(massKg : Double, heightM : Double) -> Double {
+    var bmi : Double = 0.0
+    bmi = massKg / heightM
+
+    if bmi > 25 {
+        print("overweight")
+    }else if bmi >= 18.5 && bmi <= 25.0 {
+        print("normal weight")
+    }else {
+        print("underweight")
+    }
+
+    return bmi
+}//func getBMI
+
+
+getBMI(massKg: 100, heightM: 1.77)
