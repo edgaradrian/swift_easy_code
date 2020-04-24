36fc022c68a95bc213df9e1bd0cd6728bb19d426
Edgar Adrián <adick.teddy@gmail.com>
2020-04-24 14:03:40 -0500

    Instances of EllipticalWorkout and TreadmillWorkout

--- SwiftProtocolExtensions.playground/Contents.swift
+++ SwiftProtocolExtensions.playground/Contents.swift
@@ -12,9 +12,14 @@ struct EllipticalWorkout: Exercise {
     let minutes: Double
 }//EllipticalWorkout
 
+let ellipticalWorkout = EllipticalWorkout(caloriesBurned: 335, minutes: 30)
+
 struct TreadmillWorkout: Exercise {
     let name = "Treadmill Workout"
     let caloriesBurned: Double
     let minutes: Double
     let laps: Double
 }//TreadmillWorkout
+
+let runningWorkout = TreadmillWorkout(caloriesBurned: 350, minutes: 25, laps: 10.2)
+
