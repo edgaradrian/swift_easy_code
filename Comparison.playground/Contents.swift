961c877c806e8b0b1ebcaab846fce3107d4dc320
Edgar Adrián <adick.teddy@gmail.com>
2020-04-29 09:51:00 -0500

    Testing the < function

--- Comparison.playground/Contents.swift
+++ Comparison.playground/Contents.swift
@@ -18,3 +18,8 @@ let a = Point(x: 3, y: 4)
 let b = Point(x: 3, y: 4)
 let abEqual = (a == b)
 let anNotEqual = (a != b)
+let c = Point(x: 2, y: 6)
+let d = Point(x: 3, y: 7)
+
+let cdEqual = (c == d)
+let cLessThanD = (c < d)
