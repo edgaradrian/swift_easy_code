01f16625fc376cf2b222e26a9e72911f94103653
Edgar Adrián <adick.teddy@gmail.com>
2020-04-29 10:44:13 -0500

    Swift Equatable and Comparable - Bronze and Gold Challenges

--- Comparison.playground/Contents.swift
+++ Comparison.playground/Contents.swift
@@ -12,6 +12,11 @@ struct Point: Comparable {
         return (lhs.x < rhs.x) && (lhs.y < rhs.y)
     }//<
     
+    //Bronze Challenge
+    static func +(lhs: Point, rhs: Point) -> Point {
+        return Point(x: lhs.x + rhs.x, y: lhs.y + rhs.y)
+    }//+
+    //Bronze Challenge
 }//Point
 
 let a = Point(x: 3, y: 4)
@@ -27,3 +32,30 @@ let cLessThanD = (c < d)
 let cLessThanEqualD = (c <= d)
 let cGreaterThanD = (c > d)
 let cGreaterThanEqualD = (c >= d)
+//Bronze Challenge
+let newPoint = a + b
+//Bronze Challenge
+
+//Gold Challenge
+class Person: Equatable {
+
+    let name: String
+    let age: Int
+    
+    init(name: String, age: Int) {
+        self.name = name
+        self.age = age
+    }//init
+    
+    static func == (lhs: Person, rhs: Person) -> Bool {
+        return (lhs.name == rhs.name) && (lhs.age == rhs.age)
+    }
+    
+}//class Person
+
+let p1 = Person(name: "Edgar", age: 32)
+let p2 = Person(name: "Adrian", age: 32)
+
+let people = [p1,p2]
+let index = people.index(of: p1)
+//Gold Challenge
